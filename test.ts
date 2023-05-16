029ba66591d27ba99d584fbfb746848fa15e4887
jieun <jiny273588@gmail.com>
2023-05-16 11:52:19 +0900

    void

--- test.ts
+++ test.ts
@@ -1,8 +1,12 @@
 const a = 1;
 const b = 2;
 
+function moreFunction():void{
+  console.log('hi');
+}
+//void 리턴이 없다 ->실행함수(코드를 안봐도 된다.)
 function c(a:number,b:number) : number{
   return a+b;
 }
 
-console.log(c("a",b));
+console.log(c(a,b));
